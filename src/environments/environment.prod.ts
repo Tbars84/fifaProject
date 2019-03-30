46262a6c47a924622229919f1d3e2d366464f3a2
Camilo_Tabares <camilo.tabares.84@gmail.com>
2019-03-30 11:12:41 -0500

    exposing enviroment credencials

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDOwUH30dHVhrjXzwgsCLHfMYRf-vK7kk",
+    authDomain: "datamockup.firebaseapp.com",
+    databaseURL: "https://datamockup.firebaseio.com",
+    projectId: "datamockup",
+    storageBucket: "datamockup.appspot.com",
+    messagingSenderId: "230884558630"
+  }  
+};
